fb34c018cf433b4933c6919a7b83c208a8a9e4d0
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-12-16 12:53:57 +0000

    Update Checksum generator.py

--- Checksum generator.py
+++ Checksum generator.py
@@ -8,7 +8,7 @@ user_account = (input("Enter a number: "))
 for i in user_account:
     digit = int(i) #Convert each character to an int
     runningtotal += digit * counter #Multiply digit by the current counter
-    print(f"Digit: {digit}, Weight: {counter}, Running Total: {runningtotal}")
+    print(f"Digit: {digit}, Counter: {counter}, Running Total: {runningtotal}")
     counter += 1
     next_multiple_of_11 = (runningtotal // 11 + 1) * 11 #The next multiple of 11
     checksum_digit = next_multiple_of_11 - runningtotal
